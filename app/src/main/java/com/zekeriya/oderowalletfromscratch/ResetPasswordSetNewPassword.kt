d149eb028df1024534464462c203d56d8ceaac11
zekeriyatbbas <zekeriyatbbas@outlook.com>
2025-07-29 16:08:11 +0300

    Daily update
    new pages are added, some problems of previous pages solved

--- app/src/main/java/com/zekeriya/oderowalletfromscratch/ResetPasswordSetNewPassword.kt
+++ app/src/main/java/com/zekeriya/oderowalletfromscratch/ResetPasswordSetNewPassword.kt
@@ -8,12 +8,15 @@ import android.text.Editable
 import android.text.InputType
 import android.text.Layout
 import android.text.TextWatcher
+import android.view.Gravity
 import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.zekeriya.oderowalletfromscratch.databinding.FragmentResetPasswordSetNewPasswordBinding
 
 class ResetPasswordSetNewPassword : Fragment() {
@@ -31,6 +34,27 @@ class ResetPasswordSetNewPassword : Fragment() {
 
         binding.backButtonSetNewPass.setOnClickListener {
             parentFragmentManager.popBackStack()
+        }
+
+        var frequencyChoice = "No choice"
+        binding.TextFieldFreqCon.setOnClickListener {
+            val frequencyDialogView = layoutInflater.inflate(R.layout.password_reset_frequency_dialog,null)
+            val frequencyDialog = AlertDialog.Builder(requireContext())
+                .setView(frequencyDialogView)
+                .create()
+            frequencyDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            frequencyDialog.window?.setGravity(Gravity.BOTTOM)
+            val freqViews = listOf<TextView>(frequencyDialogView.findViewById<TextView>(R.id.frequency01),
+                frequencyDialogView.findViewById<TextView>(R.id.frequency03),
+                frequencyDialogView.findViewById<TextView>(R.id.frequency06))
+            for (freqView in freqViews){
+                freqView.setOnClickListener {
+                    frequencyChoice = freqView.text.toString()
+                    binding.FreqText.setText(frequencyChoice)
+                    frequencyDialog.dismiss()
+                }
+            }
+            frequencyDialog.show()
         }
 
         var isPassVisible = false
@@ -60,30 +84,23 @@ class ResetPasswordSetNewPassword : Fragment() {
             binding.PassTextReenter.setSelection(binding.PassTextReenter.text.length)
         }
 
-        var isPasswordSixDigitAndNumber = true
+        var isPasswordSixDigitAndNumber = false
         var hasPasswordFourSameDigits = false
         var hasPasswordSequentialDigits = false
         var isPasswordSameOneOfLastThree = false
 
         binding.PassText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable?) {
                 val currentEnteredPassword = s.toString()
                 if (currentEnteredPassword.length == 6){
                     isPasswordSixDigitAndNumber = true
                     binding.circle1.setImageResource(R.drawable.checkmark)
-                }else {
-                    // wouldn't it be better if we manage this part in the KEY_DELETE part
                 }
 
-                var passwordNumbers = IntArray(10)
+                val passwordNumbers = IntArray(10)
+                hasPasswordFourSameDigits = false
                 for (number in currentEnteredPassword) {
                     val numericValue = number.digitToInt()
                     passwordNumbers[numericValue]++
@@ -101,6 +118,7 @@ class ResetPasswordSetNewPassword : Fragment() {
         })
         binding.PassText.setOnKeyListener { v, keyCode, event ->
             if(keyCode == KeyEvent.KEYCODE_DEL && event.action == KeyEvent.ACTION_DOWN) {
+                isPasswordSixDigitAndNumber = false
                 binding.circle1.setImageResource(R.drawable.circle)
                 false
             }else{
@@ -108,8 +126,37 @@ class ResetPasswordSetNewPassword : Fragment() {
             }
         }
         binding.SubmitButtonCon.setOnClickListener {
+            if (frequencyChoice == "No choice"){
+                val noChoiceView = layoutInflater.inflate(R.layout.wrong_information_login,null)
+                val noChoiceDialog = AlertDialog.Builder(requireContext()).setView(noChoiceView).create()
+                noChoiceDialog.setCanceledOnTouchOutside(false)
+                noChoiceDialog.setCancelable(false)
+                noChoiceDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                noChoiceView.findViewById<TextView>(R.id.mainErrorMessage).setText("Şifre değiştirme sıklığı seçmedin")
+                noChoiceView.findViewById<TextView>(R.id.additionalInfo).setText("Lütfen şifre değiştirme sıklığını seç")
+                noChoiceView.findViewById<Button>(R.id.dismissButton).setOnClickListener {
+                    noChoiceDialog.dismiss()
+                }
+                noChoiceDialog.show()
+            }
+
+            val doPasswordsMatch = binding.PassText.text.toString() == binding.PassTextReenter.text.toString()
+            if (!doPasswordsMatch){
+                val passMatchView = layoutInflater.inflate(R.layout.wrong_information_login,null)
+                val passMatchDialog = AlertDialog.Builder(requireContext()).setView(passMatchView).create()
+                passMatchDialog.setCanceledOnTouchOutside(false)
+                passMatchDialog.setCancelable(false)
+                passMatchDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                passMatchView.findViewById<TextView>(R.id.mainErrorMessage).setText("Girdiğin şifreler uyuşmuyor")
+                passMatchView.findViewById<TextView>(R.id.additionalInfo).setText("Lütfen şifreni kontrol et")
+                passMatchView.findViewById<Button>(R.id.dismissButton).setOnClickListener {
+                    passMatchDialog.dismiss()
+                }
+                passMatchDialog.show()
+            }
             if (isPasswordSixDigitAndNumber && !isPasswordSameOneOfLastThree &&
-                !hasPasswordFourSameDigits && !hasPasswordSequentialDigits) {
+                !hasPasswordFourSameDigits && !hasPasswordSequentialDigits &&
+                frequencyChoice != "No choice" && doPasswordsMatch) {
                 val passwordResetSuccessView = layoutInflater.inflate(R.layout.password_reset_success,null)
                 val passwordResetSuccessDialog = AlertDialog.Builder(requireContext())
                     .setView(passwordResetSuccessView).create()
--- app/src/main/java/com/zekeriya/oderowalletfromscratch/SignUpIDInformation.kt
+++ app/src/main/java/com/zekeriya/oderowalletfromscratch/SignUpIDInformation.kt
@@ -1,15 +1,21 @@
 package com.zekeriya.oderowalletfromscratch
 
+import android.app.AlertDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.zekeriya.oderowalletfromscratch.databinding.ActivityMainBinding
 import com.zekeriya.oderowalletfromscratch.databinding.FragmentSignUpIDInformationBinding
 
 class SignUpIDInformation : Fragment() {
     private lateinit var binding: FragmentSignUpIDInformationBinding
+    private var IDInformationErrorTypes = listOf<String>("No name", "No surname", "ID length")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,5 +28,44 @@ class SignUpIDInformation : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.OuterShell.setOnClickListener {
+            val nameText = binding.NameInput.text.toString()
+            val surnameText = binding.SurnameInput.text.toString()
+            val IDText = binding.TCInput.text.toString()
+            if (IDText.length == 11 && nameText.length > 0 && surnameText.length > 0){
+                parentFragmentManager.beginTransaction().replace(R.id.Splash,
+                    SignUpContactInformation()).commit()
+            }
+            if (nameText.length == 0){
+                this.createErrorDialog(IDInformationErrorTypes[0])
+            }else if (surnameText.length == 0){
+                this.createErrorDialog(IDInformationErrorTypes[1])
+            }else if (IDText.length != 11){
+                this.createErrorDialog(IDInformationErrorTypes[2])
+            }
+        }
+
+    }
+    fun createErrorDialog(errorType: String){
+        if (!IDInformationErrorTypes.contains(errorType)){
+            return
+        }
+        val errorDialogView = layoutInflater.inflate(R.layout.wrong_information_login,null)
+        val errorDialog = AlertDialog.Builder(requireContext()).setView(errorDialogView).create()
+        errorDialog.setCancelable(false)
+        errorDialog.setCanceledOnTouchOutside(false)
+        errorDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        errorDialogView.findViewById<Button>(R.id.dismissButton).setOnClickListener {
+            errorDialog.dismiss()
+        }
+        when(errorType){
+            IDInformationErrorTypes[0] ->
+                errorDialogView.findViewById<TextView>(R.id.additionalInfo).text = "Lütfen adını gir"
+            IDInformationErrorTypes[1] ->
+                errorDialogView.findViewById<TextView>(R.id.additionalInfo).text = "Lütfen soyadını gir"
+            IDInformationErrorTypes[2] ->
+                errorDialogView.findViewById<TextView>(R.id.additionalInfo).text = "TC kimlik numaran 11 haneli olmalı"
+        }
+        errorDialog.show()
     }
 }
